928b5ffd1cd1deef482dc88319a1f2d7c7ab9253
Manuel Pasieka <mapa17>
2021-11-24 18:06:23 +0100

    WiP

--- src/ppe/main.py
+++ src/ppe/main.py
@@ -3,8 +3,13 @@ import numpy as np
 def hello():
     print(f"Hello world!")
 
+def print_artefact():
+    with open('artifact.txt', 'r') as f:
+        text = '\n'.join(f.readlines())
+        print(f"Artifact content:\n{text}")
+
 def main():
-    hello()
+    print_artefact()
 
 if __name__ == '__main__':
     main()
