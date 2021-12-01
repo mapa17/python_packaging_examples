cc92a8ce4c0315103bdbe637e6a596b30166fb4a
Manuel Pasieka <mapa17>
2021-12-01 18:07:47 +0100

    WiP

--- src/ppe/main.py
+++ src/ppe/main.py
@@ -1,10 +1,16 @@
 import numpy as np
 
+import os
+import pkg_resources
+
+artifacts_path = os.path.abspath(pkg_resources.resource_filename(__name__, '../../../../ppe/artifacts'))
+
 def hello():
     print(f"Hello world!")
 
 def print_artefact():
-    with open('artifact.txt', 'r') as f:
+    from pudb import set_trace as st; st()
+    with open(os.path.join(artifacts_path, 'artifact.txt'), 'r') as f:
         text = '\n'.join(f.readlines())
         print(f"Artifact content:\n{text}")
 
