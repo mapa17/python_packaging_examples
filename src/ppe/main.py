084c834a1219a7e86970b28f985ea24544edefec
Manuel Pasieka <manuel.pasieka@protonmail.ch>
2021-12-16 17:28:51 +0100

    Updating main.py

--- src/ppe/main.py
+++ src/ppe/main.py
@@ -3,16 +3,21 @@ import numpy as np
 import os
 import pkg_resources
 
-artifacts_path = os.path.abspath(pkg_resources.resource_filename(__name__, '../../../../ppe/artifacts'))
+artifacts1_path = os.path.abspath(pkg_resources.resource_filename(__name__, '../../../../ppe/artifacts/artifact.txt'))
+artifacts2_path = os.path.abspath(pkg_resources.resource_filename(__name__, 'data/artifact2.txt'))
 
 def hello():
     print(f"Hello world!")
 
 def print_artefact():
-    from pudb import set_trace as st; st()
-    with open(os.path.join(artifacts_path, 'artifact.txt'), 'r') as f:
+    with open(artifacts1_path, 'r') as f:
         text = '\n'.join(f.readlines())
-        print(f"Artifact content:\n{text}")
+        print(f"Artifact 1 content:\n{text}")
+
+    with open(artifacts2_path, 'r') as f:
+        text = '\n'.join(f.readlines())
+        print(f"Artifact 2 content:\n{text}")
+
 
 def main():
     print_artefact()
